ea871044fdf498861c51adec7e17923b0f9145a0
rudrakumar2012 <manaw10071994@gmail.com>
2024-02-27 11:22:30 +0530

    Work added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Main from "./components/Main";
 import Sidenav from "./components/Sidenav";
+import Work from "./components/Work";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div>
         <Sidenav />
         <Main />
+        <Work />
       </div>
     </>
   );
